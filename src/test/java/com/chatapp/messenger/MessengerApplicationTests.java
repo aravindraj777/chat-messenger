5bb0abaf2114d3898abfad5cd43c6389aa54212d
Aravind <aravindmunna240@gmail.com>
2024-03-07 13:02:39 +0530

    Initial commit

---
+++ src/test/java/com/chatapp/messenger/MessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
